7817d398c36e554b133bba8f2d5cbe5b53a0a245
Alina Moskieva <alinamoskieva@gmail.com>
2016-04-20 20:05:13 +0300

    amended

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,5 @@ class Page < ActiveRecord::Base
   validates :title, :body, :user_id, :category_id, presence: true
   belongs_to :user
   has_many :comments
-  has_one :category
+  belongs_to :category
 end
