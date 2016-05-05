40931bf4aacfac1c8a995fde33b1e9daa092e03e
Alina Moskieva <alinamoskieva@gmail.com>
2016-05-05 16:54:55 +0300

    routes amended

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#index"
   resources :comments
   resources :pages
-  resources :categories
+  resources :categories, only: [:index]
   resources :profile, only: [:index]
 
 
