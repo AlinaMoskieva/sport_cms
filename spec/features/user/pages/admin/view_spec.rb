0326dca10cb04543979a61644c867760793dd19a
Alina Moskieva <alinamoskieva@gmail.com>
2016-05-19 19:20:22 +0300

    Added rspec for view page as user

--- spec/features/user/pages/admin/view_spec.rb
+++ spec/features/user/pages/admin/view_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
-feature "View page" do
-  let!(:user) { create :user }
+feature "View page as administrator" do
+   include_context "current user signed in"
+  let!(:user) { current_user }
   let!(:category) { create :category }
   let!(:site_page) { create :page, user: user, category: category }
 
@@ -16,7 +17,7 @@ feature "View page" do
     expect(page).to have_content(site_page.visitors)
     expect(page).to have_content(site_page.comments.count)
   end
-  scenario "view page content" do
+  scenario "view page content as administrator" do
     visit root_path
 
     click_link site_page.title
@@ -25,12 +26,8 @@ feature "View page" do
     expect(page).to have_content(site_page.user.full_name)
     expect(page).to have_content(site_page.category.category)
     expect(page).to have_content(site_page.visitors)
-    expect(page).to_not have_link("Delete")
-    expect(page).to_not have_link("Edit page")
-    expect(page).to_not have_link("Subscribe")
-    expect(page).to_not have_link("Edit")
+    expect(page).to have_link("Subscribe")
     expect(page).to have_link("Back")
-    expect(page).to_not have_field("body")
     expect(page).to have_link("Categories")
   end
 end
