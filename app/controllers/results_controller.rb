1b1139d466c47c801c73175e20cd6d36fa71dcf5
Alina Moskieva <alinamoskieva@gmail.com>
2016-05-21 18:27:11 +0300

    added ability to create result

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,13 +1,28 @@
 class ResultsController < ApplicationController
-  expose(:result)
-  ## TODO expose
+  expose(:result, attributes: :result_params)
+  expose(:results) { Result.all }
   expose(:categories) { Category.all }
 
   def index
     @results = Result.all
   end
 
+  def create
+    result = Result.new(result_params)
+
+    respond_to do |format|
+      if result.save
+        format.html{ redirect_to :back, notice: "The person has been created!" }
+      else
+        format.html{ render 'new' }
+      end
+    end
+  end
+
   def show
-    binding.pry
+  end
+
+  def result_params
+    params.require(:result).permit(:category_id, :country, :result, :prize)
   end
 end
