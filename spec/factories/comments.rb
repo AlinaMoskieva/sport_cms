7969d0a0a9b555c36daa49ed5fef73f6f67940d5
Alina Moskieva <alinamoskieva@gmail.com>
2016-06-01 12:31:00 +0300

    view spec added

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :comment do
     body "Test"
     user
+    page
   end
 end
--- spec/features/user/pages/view_spec.rb
+++ spec/features/user/pages/view_spec.rb
@@ -7,32 +7,17 @@ feature "View page as administrator" do
   let!(:category) { create :category }
   let!(:site_page) { create :page, user: user, category: category }
 
-  describe "view page" do
+  describe "view index page" do
     before { visit "/" }
 
-    it "should have content" do
+    it "has content" do
       expect(page).to have_content(site_page.title)
       expect(page).to have_content(site_page.visitors)
     end
 
-    it "should have ralation with other enities" do
+    it "has ralation with other enities" do
      expect(page).to have_content(site_page.category.category)
      expect(page).to have_content(site_page.comments.count)
-    end
-  end
-
-
-  context "view page content as administrator" do
-    before { visit page_path(site_page) }
-
-    it "should have page content" do
-      expect(page).to have_content(site_page.body)
-      expect(page).to have_content(site_page.visitors)
-      expect(page).to have_link("Back")
-    end
-
-    it "should have relation with other enities" do
-      expect(page).to have_content(site_page.category.category)
     end
   end
 end
