07af9a2985ea16a3c737256f7b37b9049121b12c
Alina Moskieva <alinamoskieva@gmail.com>
2016-06-14 13:49:11 +0300

    create subscription spec refactored

--- spec/features/user/users_category/create_subscription_spec.rb
+++ spec/features/user/users_category/create_subscription_spec.rb
@@ -17,14 +17,14 @@ feature "Create page" do
       expect(page).to have_link("sub link")
     end
 
-    it "I can create subscription" do
+    scenario "I can create subscription" do
       expect(page).to have_content("Subscribe")
       click_link("sub link")
       visit page_path(site_page)
       expect(page).to have_content("Unsubscribe")
     end
 
-    it "I can destroy subscription" do
+    scenario "I can destroy subscription" do
       admin_user.subscribed_categories = Array.new
       click_link("sub link")
       expect(page).to have_content("Unsubscribe")
@@ -35,24 +35,24 @@ feature "Create page" do
   context "As user I am able create subscription" do
     let!(:user) { create :user, :user }
 
-    background do
+    before {
       login_as user
       visit page_path(site_page)
-    end
+    }
 
 
     it "has link to subscribe" do
       expect(page).to have_link("sub link")
     end
 
-    it "I can create subscription" do
+    scenario "I can create subscription" do
       expect(page).to have_content("Subscribe")
       click_link("sub link")
       visit page_path(site_page)
       expect(page).to have_content("Unsubscribe")
     end
 
-    it "I can destroy subscription" do
+    scenario "I can destroy subscription" do
       admin_user.subscribed_categories = Array.new
       click_link("sub link")
       expect(page).to have_content("Unsubscribe")
@@ -60,10 +60,10 @@ feature "Create page" do
       expect(page).not_to have_content("Unsubscribe")
     end
   end
-  context "As visitor I cant create/destroy subscription" do
-    before { visit page_path(site_page) }
 
+  context "As visitor I cant create/destroy subscription" do
     it "has link to subscribe" do
+      visit page_path(site_page)
       expect(page).not_to have_link("sub link")
     end
   end
