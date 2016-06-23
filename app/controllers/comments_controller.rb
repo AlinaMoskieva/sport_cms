731046ad9ad454f725b76e6464eb4ab657b3b9d9
Alina Moskieva <alinamoskieva@gmail.com>
2016-06-23 22:33:36 +0300

    Added ability to send notifications to users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if comment.save
+        notify(comment.body) if comment.body.include?('@')
         format.html{ redirect_to comment.page, notice: 'Comment was successfully created.' }
       else
         format.html{ render :new }
@@ -52,5 +53,14 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body, :user_id, :page_id)
+  end
+
+  def notify(str)
+    @str = str.slice((comment.body.index('@') + 1)..comment.body.length)
+    @nick = @str.slice(0..(@str.index(' ').to_i - 1))
+    @str = @str.slice((@str.index(@nick.last) - 1 )..comment.body.length)
+    Notification.create(comment_id: comment.id,
+      user_id: User.where(nickname: @nick).ids.first )
+    notify(@str) if @str.include?('@')
   end
 end
