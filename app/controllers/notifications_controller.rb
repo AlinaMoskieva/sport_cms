d8438f0d05b07f68e43a4bad809b3be098377459
Alina Moskieva <alinamoskieva@gmail.com>
2016-06-23 22:45:04 +0300

    Decorator added and Index page stylized

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,23 +2,7 @@ class NotificationsController < ApplicationController
   respond_to :html
 
   expose(:notification, attributes: :notification_params)
-  expose(:notifications) { Notification.all }
-
-  def create
-    notification.user_id = current_user.id
-    flash[:notice] = 'Notification was successfully created.' if notification.save
-    respond_with(notification)
-  end
-
-  def update
-    flash[:notice] = 'Notification was successfully updated.' if notification.save
-    respond_with(notification)
-  end
-
-  def destroy
-    notification.destroy
-    respond_with(notification)
-  end
+  expose_decorated(:notifications) { Notification.where(user_id: current_user.id) }
 
   private
 
--- app/decorators/notification_decorator.rb
+++ app/decorators/notification_decorator.rb
@@ -1,3 +1,26 @@
 class NotificationDecorator < ApplicationDecorator
-  delegate_all
+
+  def sender
+    User.find(Comment.find(object.comment_id).user_id).full_name
+  end
+
+  def sender_email
+    User.find(Comment.find(object.comment_id).user_id).email
+  end
+
+  def comment
+    Comment.find(object.comment_id).body.html_safe
+  end
+
+  def date
+    object.created_at.strftime("%m.%d.%Y at %I:%M%P")
+  end
+
+  def photo
+    User.find(Comment.find(object.comment_id).user_id).image
+  end
+
+  def page
+    Page.find(Comment.find(object.comment_id).page_id)
+  end
 end
