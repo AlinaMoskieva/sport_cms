b01fb4d86ed23ea81869718f051709617b477769
Alina Moskieva <alinamoskieva@gmail.com>
2016-06-29 18:17:33 +0300

    Remove password

--- db/seeds/admins.seeds.rb
+++ db/seeds/admins.seeds.rb
@@ -1,7 +1,6 @@
 3.times do
   User.create!(email: Faker::Internet.email,
               full_name:  Faker::Name.name ,
-              password: Faker::Internet.password(10, 20, true),
               nickname: Faker::Name.first_name,
               role: "administrator" )
 end
