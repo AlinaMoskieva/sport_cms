41cbd05def326b581b2cead5be809546285621dc
Alina Moskieva <alinamoskieva@gmail.com>
2016-06-30 15:24:20 +0300

    add id to page seed

--- db/seeds/pages.seeds.rb
+++ db/seeds/pages.seeds.rb
@@ -1,8 +1,9 @@
 after :categories do
 admin = User.find(1)
   20.times do |page|
-    admin.pages.create!(body: Faker::Lorem.paragraphs,
-                      title: Faker::Lorem.sentence(3, true, 4),
-                      category_id: Faker::Number.between(1, 10))
+    Page.create!(body: Faker::Lorem.paragraphs,
+                        title: Faker::Lorem.sentence(3, true, 4),
+                        category_id: Faker::Number.between(1, 10),
+                        user_id: Faker::Number.between(1, 3))
   end
 end
