71216fa0e43057c5fe7a42db83ac2323a2d49734
Alina Moskieva <alinamoskieva@gmail.com>
2016-06-30 15:52:41 +0300

    Add seed for users

--- db/seeds/pages.seeds.rb
+++ db/seeds/pages.seeds.rb
@@ -3,6 +3,6 @@ after :categories do
     Page.create!(body: Faker::Lorem.paragraphs,
                         title: Faker::Lorem.sentence(3, true, 4),
                         category_id: Faker::Number.between(1, 10),
-                        user_id: Faker::Number.between(1, 3))
+                        user_id: Faker::Number.between(1, 23))
   end
 end
