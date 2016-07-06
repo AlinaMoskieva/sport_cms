b0e982c26e53a840781623c20aedfcf79afea833
Alina Moskieva <alinamoskieva@gmail.com>
2016-07-06 18:01:12 +0300

    update controllers with using before_action instead of before_filter

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,6 @@
 module Admin
   class UsersController < ApplicationController
-    before_filter :authenticate_user!
-    #before_action :authorization_admin
+    before_action :authenticate_user!
     expose(:admin)
     expose(:users) { User.all.order(id: :asc).page params[:page] }
     expose(:user)
