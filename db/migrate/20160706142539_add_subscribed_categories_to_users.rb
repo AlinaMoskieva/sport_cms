085a614f7fa12804b6706cdbb664e28b15b7c159
Alina Moskieva <alinamoskieva@gmail.com>
2016-07-07 12:25:45 +0300

    Remove useless migrations

--- db/migrate/20160706142539_add_subscribed_categories_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddSubscribedCategoriesToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :subscribed_categories, :text
-    remove_column :users, :role, :integer
-    add_column :users, :role, :string
-  end
-end
