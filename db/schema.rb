f2ad5de08bf714787d60b551b6bd30df3189fdcc
Alina Moskieva <alinamoskieva@gmail.com>
2016-07-07 12:47:26 +0300

    Return to another schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706184628) do
+ActiveRecord::Schema.define(version: 20160707093830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,8 +87,8 @@ ActiveRecord::Schema.define(version: 20160706184628) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",     null: false
-    t.string   "encrypted_password",     default: "",     null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -107,7 +107,7 @@ ActiveRecord::Schema.define(version: 20160706184628) do
     t.string   "image"
     t.string   "nickname"
     t.text     "subscribed_categories"
-    t.string   "role",                   default: "user"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
