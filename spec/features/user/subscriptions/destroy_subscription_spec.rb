e20407e271ec853303ed49e01ef08e5d814509af
Alina Moskieva <alinamoskieva@gmail.com>
2016-07-12 17:31:10 +0300

    Added spec for destroy subscription method

---
+++ spec/features/user/subscriptions/destroy_subscription_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+feature "Destroy subscription" do
+  let!(:category) { create :category }
+  let!(:user) { create :user, :administrator }
+  let!(:site_page) { create :page, user: user, category: category }
+  let!(:subscription) {create :subscription, user: user, category: category}
+
+  context "Logged user" do
+    before do
+      login_as user
+      visit page_path(site_page)
+    end
+
+    it "has link to Unsubscribe" do
+      expect(page).to have_link("Unsubscribe")
+    end
+
+    scenario "can Destroy subscription" do
+      expect do
+        click_link "Unsubscribe"
+      end.to change{ Subscription.count }.by(-1)
+    end
+  end
+  context "As visitor" do
+    it "has not link to Unsubscribe" do
+      visit page_path(site_page)
+      expect(page).not_to have_link("Unsubscribe")
+    end
+  end
+end
