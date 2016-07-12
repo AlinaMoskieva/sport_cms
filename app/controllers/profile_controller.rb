751a607275aeffdcafed3b918e1c3bd1e4e1541f
Alina Moskieva <alinamoskieva@gmail.com>
2016-07-12 17:48:12 +0300

    Add my subscriptions block to profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,5 @@
 class ProfileController < ApplicationController
   expose_decorated(:user) { current_user }
   expose(:pages) { user.pages.includes(:category).order(created_at: :desc).page params[:page] }
+  expose(:subscriptions) { Category.where("id IN (?)", current_user.subscriptions.map(&:category_id)) }
 end
