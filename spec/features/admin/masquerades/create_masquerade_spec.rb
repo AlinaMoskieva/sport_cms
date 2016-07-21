580ba910a2fa96094005d526e0e835cd6238dad5
Alina Moskieva <alinamoskieva@gmail.com>
2016-07-21 13:14:24 +0300

    Add spec for masquerades#create

---
+++ spec/features/admin/masquerades/create_masquerade_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "Create masquerade" do
+  let!(:admin_user) { create :user, :administrator }
+  let!(:user) { create :user, :user }
+
+  before do
+    login_as admin_user
+    visit admin_users_path
+  end
+
+  describe "#create" do
+    it "exist link 'new session as ..'" do
+      expect(page).to have_link("New session as #{ user.email}".truncate(22))
+    end
+
+   scenario "Create masquerade" do
+      click_link "New session as #{user.email}".truncate(22)
+      visit root_path
+      expect(page).to have_content(user.full_name)
+      expect(page).to have_link("Stop Masquerading")
+    end
+  end
+end
